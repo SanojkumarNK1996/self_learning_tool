85af55a25e13f592faa1a113e23921d06d8b7938
Naveenkumar A <naveenkumar@maritech.com>
2025-09-01 16:34:07 +0530

    feat(beACoder) base code and setup done with some base screen's like login etc.

--- beacoder-source/beacoder-frontend/beACoder/src/app/@pages/login/login.component.ts
+++ beacoder-source/beacoder-frontend/beACoder/src/app/@pages/login/login.component.ts
@@ -1,16 +1,33 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'bac-login',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   private readonly router = inject(Router);
 
-  protected login() {
-    this.router.navigate(['/user']);
+  protected navigate(path: string) {
+    this.router.navigate([path]);
   }
 }
